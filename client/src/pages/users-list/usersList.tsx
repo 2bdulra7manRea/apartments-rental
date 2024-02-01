98d77f355bc2e5b09504926aff773619d5d8280f
abdelrahmanMosa <abdomosa987@gmail.com>
2024-02-01 14:41:04 +0200

    added users list

--- client/src/pages/users-list/usersList.tsx
+++ client/src/pages/users-list/usersList.tsx
@@ -1,3 +1,71 @@
+import { Button, Table } from "antd";
+
 export function UsersListPage() {
-  return <></>;
+  const dataSource = [
+    {
+      key: "1",
+      username: "Mike",
+      email: "das@gmail.com",
+      role: "realtor",
+    },
+    {
+      key: "2",
+      username: "dMike",
+      email: "das@gmail.com",
+      role: "realtor",
+    },
+    {
+      key: "4",
+      username: "moz",
+      email: "das@gmail.com",
+      role: "client",
+    },
+    {
+      key: "3",
+      username: "sMike",
+      email: "das@gmail.com",
+      role: "admin",
+    },
+  ];
+
+  const columns = [
+    {
+      title: "Username",
+      dataIndex: "username",
+      key: "username",
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "Role",
+      render: (row: any) => {
+        return (
+          <>
+            <span
+              style={{
+                backgroundColor: "green",
+                color: "white",
+                borderRadius: "20px",
+                display: "inline-block",
+                padding: "5px 10px",
+              }}
+            >
+              {row.role}
+            </span>
+          </>
+        );
+      },
+    },
+  ];
+  return (
+    <div className="p-4">
+      <h1 className="m-4">Users</h1>
+      <div>
+        <Table dataSource={dataSource} columns={columns} />;
+      </div>
+    </div>
+  );
 }
