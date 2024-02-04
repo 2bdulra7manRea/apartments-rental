cd9e828090ef1fc1b5df387fd62a7c3700a7e5a3
abdelrahmanMosa <abdomosa987@gmail.com>
2024-02-04 14:34:50 +0200

    added middleware interceptor api

---
+++ server/src/common/interceptor/middleware.interceptor.ts
@@ -0,0 +1,33 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+
+export interface Response<T> {
+  data: T;
+}
+
+@Injectable()
+export class MiddlewareInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      catchError((err) =>
+        throwError(() => {
+          return new HttpException(err, HttpStatus.BAD_REQUEST);
+        }),
+      ),
+      map((data) => ({ data })),
+    );
+  }
+}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MiddlewareInterceptor } from './common/interceptor/middleware.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('/api');
+  app.useGlobalInterceptors(new MiddlewareInterceptor());
   await app.listen(4000);
 }
 bootstrap();
